6c3a2eb7c6970d54807425dde63fe09a57dc3862
shaieinav <shai.einav@gmail.com>
2021-01-26 23:38:23 +0200

    max results now includes replies count

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# youtube-comments-crawler
-Creating a YouTube video comments API to crawl from
+# YouTube Comments Crawler
+
+## Description
+Creating a YouTube video comments API to crawl from and analyze comments and links.
+
+## Prerequisites
+
+Install NPM
+```sh
+sudo apt install npm
+```
+
+Or upgrade NPM if needed
+```sh
+npm install -g npm@latest
+```
+
+## Installation
+
+1. Clone the repository
+   ```sh
+   git clone https://github.com/shaieinav/youtube-comments-crawler.git
+   ```
+2. cd to the repo directory
+   ```sh
+   cd youtube-comments-crawler
+   ```
+3. Install NPM packages
+   ```sh
+   npm i
+   ```
+4. Run the project
+   ```sh
+   npm start
+   ```
--- index.js
+++ index.js
@@ -2,41 +2,46 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const port = 3000;
 
+const PORT = 3000;
 const BASE_API = 'https://youtube.googleapis.com/youtube/v3';
 const KEY = 'AIzaSyCTmq_2rYz3nvlBy4wHTyWrQ9HIwpA21IA';
 
 app.use(express.json());
 
+// HTTP GET request to fetch the maximum results of comments
 async function getComments(videoId, maxResults) {
     try {
         const response = await axios.get(BASE_API + '/commentThreads', { params : { videoId: `${videoId}`,
                                                                                     maxResults: `${maxResults}`,
-                                                                                    part: 'snippet,replies',
+                                                                                    part: 'snippet',
                                                                                     key: KEY } });
         return response.data;
-    } catch (error) {
-        console.log("Error while making an HTTP GET call to retrieve all comments, in getComments().", error);
+    }
+    catch (error) {
+        console.log("Error while making an HTTP GET call to retrieve all comments, in getComments().\n", error);
     }
 }
 
-async function getCommentReplies(parentId) {
+// HTTP GET request to fetch the maximum results of comment replies
+async function getCommentReplies(parentId, maxResults) {
     try {
         const response = await axios.get(BASE_API + '/comments', { params : { parentId: `${parentId}`,
+                                                                              maxResults: `${maxResults}`,
                                                                               part: 'snippet',
                                                                               key: KEY } });
         return response.data;
     }
     catch (error) {
-        console.log("Error while making an HTTP GET call to retrieve all replies of a comments, in getCommentReplies().", error);
+        console.log("Error while making an HTTP GET call to retrieve all replies of a comments, in getCommentReplies().\n", error);
     }
 }
 
-async function addReplies(id) {
+// adding maximum number of replies to a comment
+async function addReplies(id, maxResults) {
     try {
         const replies = [];
-        const commentRepliesData = await getCommentReplies(id);
+        const commentRepliesData = await getCommentReplies(id, maxResults);
     
         commentRepliesData.items.forEach((reply) => {
             replies.push(reply.snippet.textOriginal);
@@ -45,56 +50,75 @@ async function addReplies(id) {
         return replies;
     }
     catch (error) {
-        console.log("Error while adding replies to a comment, in addReplies().", error);
+        console.log("Error while adding replies to a comment, in addReplies().\n", error);
     }
 }
 
-async function processComment(comment) {
+// processing and returning a comment and its replies, if exist
+async function processComment(comment, totalNumOfComments, numOfReplies, maxResults) {
     try {
         const commentText =  comment.snippet.topLevelComment.snippet.textOriginal;
         const numOfCommentReplies =  comment.snippet.totalReplyCount;
+        const maxResultsOfReplies = Math.min(numOfCommentReplies, maxResults - totalNumOfComments - 1);
         let commentReplies = [];
         
-        if (numOfCommentReplies > 0) {
-            commentReplies = await addReplies(comment.id);
+        // only add replies if we have space
+        if (numOfCommentReplies > 0 && maxResultsOfReplies > 0) { 
+            commentReplies = await addReplies(comment.id, maxResultsOfReplies);
         }
+        
+        // update the count of comments and replies
+        totalNumOfComments += 1 + maxResultsOfReplies;
+        numOfReplies += maxResultsOfReplies;
     
-        return {text: commentText, replies: commentReplies};
+        return [{text: commentText, replies: commentReplies}, totalNumOfComments, numOfReplies];
     }
     catch (error) {
-        console.log("Error while processing a comment, in processComment().", error);
+        console.log("Error while processing a comment, in processComment().\n", error);
     }
 }
 
 app.get('/comments', async (request, response) => {
+    try {
+        // make video id and maximum result a requiered query parameter
+        const videoId = request.query.videoId;
+        const maxResults = request.query.maxResults;
 
-    const videoId = request.query.videoId;
-    const maxResults = request.query.maxResults;
-    let numOfComments = 0;
+        let totalNumOfComments = 0, numOfReplies = 0, commentRes = {};
+    
+        // object to hold all necessary comments data to send to the endpoint
+        const allComments = {
+            videoId: `${videoId}`,
+            maxResults: `${maxResults}`,
+            numOfComments: 0,
+            numOfReplies: 0,
+            totalNumOfComments: 0,
+            comments: []
+        };
 
-    const commentsData = await getComments(videoId, maxResults);
 
-    const allComments = {
-        videoId: `${videoId}`,
-        maxResults: `${maxResults}`,
-        numOfComments: 0,
-        comments: []
-    };
+            // get the maximum number of comments from the specified video
+            const commentsData = await getComments(videoId, maxResults);
+        
+            // for every comment, process it and push it to the comments object
+            for (const comment of commentsData.items) {
+                if (totalNumOfComments < maxResults) {
+                    [commentRes, totalNumOfComments, numOfReplies] = await processComment(comment, totalNumOfComments, numOfReplies, maxResults);
+                    allComments['comments'].push(commentRes);
+                }
+            }
+    
+        // update count of comments and replies
+        allComments.numOfComments = totalNumOfComments - numOfReplies;
+        allComments.numOfReplies = numOfReplies;
+        allComments.totalNumOfComments = totalNumOfComments;
 
-    for (const comment of commentsData.items) {
-        const commentRes = await processComment(comment);
-        allComments['comments'].push(commentRes);
-        numOfComments += 1 + comment.snippet.totalReplyCount;
-    }
-
-    allComments.numOfComments = numOfComments;
-
-    if (!commentsData) {
-        response.status(200).end("Comments not found.");
-    }
-    else {
         response.status(200).end(JSON.stringify(allComments));
+    }
+    catch (error) {
+        response.status(404).end("Page Not Found!");
+        console.log(`Error while rendering comments json data at http://localhost:${PORT}, in app.get('/comments', async (request, response).\n`, error);
     }
 });
 
-app.listen(port, () => console.log(`Express server currently running on port ${port}. Server started! At http://localhost:${port}`));
+app.listen(PORT, () => console.log(`Express server currently running on port ${PORT}. Server started! At http://localhost:${PORT}`));
